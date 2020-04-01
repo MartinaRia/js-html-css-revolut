30ef0508407d0b97f09875d1d826915c77db157a
Martina <martina.ria@hotmail.com>
2020-04-01 22:01:55 +0200

    All basic complete

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
 $( document ).ready(function() {
 
-  $('.sub-menu').hide();
-
-
+  // NAVBAR DESKTOP
   $('.link-links').mouseenter(
     function(){
       $('.sub-menu', this).show();
       $('.sub-menu', this).removeClass('off');
-      $('.off').hide()
+      $('.off').hide();
     }
   ).mouseleave(
     function(){
       $('.sub-menu', this).addClass('off');
     }
-  )
+  );
 
   $( document ).click(
     function(){
       $('.sub-menu').hide()
     }
+  );
+
+  // NAVBAR MOBILE
+
+  $('.icon-hamburger').click(
+    function(){
+      $('.menu-mobile').fadeToggle()
+    }
+
   )
+
+  $('.mob-link-links').click(
+    function(){
+      $('.mob-sub-menu').addClass('off');
+      $('.mob-sub-menu', this).addClass('on').removeClass('off');
+      $('.on').slideToggle();
+      $('.off').hide();
+    }
+  );
 
 
 
